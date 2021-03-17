83ea40afcecb8ee07a59e25e351de350c2c81214
Jackson Barreto <jackson.barreto@gmail.com>
2021-03-17 10:03:31 +0000

    Feat: Inheritance of the "Serializable" interface has been implemented.

--- src/main/java/bll/valueObjects/IEmail.java
+++ src/main/java/bll/valueObjects/IEmail.java
@@ -1,5 +1,6 @@
 package bll.valueObjects;
 
+import java.io.Serializable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,7 +10,7 @@ import java.util.regex.Pattern;
  * Ensures that a valid email is maintained throughout the execution,
  * in accordance with RFC2822 and RFC1035.
  */
-public interface IEmail {
+public interface IEmail extends Serializable {
     Pattern REGEX_EMAIL_FORMAT = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])",
             Pattern.CASE_INSENSITIVE);
     String SEPARATOR = "@";
