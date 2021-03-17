d14d4b85f831530c2e1150d1da81098d64e18bb4
Jackson Barreto <jackson.barreto@gmail.com>
2021-03-17 09:45:24 +0000

    Feat: new static method (isPlainTextEmailInvalid) was created to provide better semantic readability.

--- src/main/java/bll/valueObjects/IEmail.java
+++ src/main/java/bll/valueObjects/IEmail.java
@@ -10,7 +10,8 @@ import java.util.regex.Pattern;
  * in accordance with RFC2822 and RFC1035.
  */
 public interface IEmail {
-    Pattern REGEX_EMAIL_FORMAT = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", Pattern.CASE_INSENSITIVE);
+    Pattern REGEX_EMAIL_FORMAT = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])",
+            Pattern.CASE_INSENSITIVE);
     String SEPARATOR = "@";
     /**
      * Returns the local part of the email address.
@@ -74,5 +75,18 @@ public interface IEmail {
             return false;
         Matcher matcher = REGEX_EMAIL_FORMAT.matcher(email);
         return matcher.find();
+    }
+
+    /**
+     * Indicates whether an e-mail address is invalid, according to the standards of RFC2822 and RFC1035.
+     *
+     * @param email a email address, complete, in plain text.
+     * @return {@code true} if the email address dont follows the standards of RFC2822 and RFC1035.
+     */
+    static boolean isPlainTextEmailInvalid(String email) {
+        if (email == null)
+            return true;
+        Matcher matcher = REGEX_EMAIL_FORMAT.matcher(email);
+        return !matcher.find();
     }
 }
