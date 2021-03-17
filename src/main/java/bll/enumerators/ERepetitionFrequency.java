7708e7dc7e8926055ffb417f5c3f47947d240333
Jackson Barreto <jackson.barreto@gmail.com>
2021-03-17 10:28:38 +0000

    Feat: Enumerator "RepetitionFrequency" implemented.

---
+++ src/main/java/bll/enumerators/ERepetitionFrequency.java
@@ -0,0 +1,21 @@
+package bll.enumerators;
+
+import java.io.Serializable;
+
+public enum ERepetitionFrequency implements Serializable {
+    WEEKLY(1),
+    FORTNIGHTLY(2),
+    MONTHLY(3),
+    QUARTERLY(4),
+    YEARLY(5);
+
+    private int ID;
+
+    ERepetitionFrequency(int ID) {
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+}
