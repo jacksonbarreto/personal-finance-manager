24482f80f186523d60be1f6f3362a997aeb9a24e
Jackson Barreto <jackson.barreto@gmail.com>
2021-03-17 10:22:14 +0000

    Refactor: Renamed to fit the nomenclature standard.

--- src/main/java/bll/enumerators/ERole.java
+++ src/main/java/bll/enumerators/ERole.java
@@ -2,13 +2,15 @@ package bll.enumerators;
 
 import java.io.Serializable;
 
-public enum Role implements Serializable {
+public enum ERole implements Serializable {
 
-    SIMPLE(1), PREMIUM(2), ADMIN(3);
+    SIMPLE(1),
+    PREMIUM(2),
+    ADMIN(3);
 
     private final int roleID;
 
-    Role(int roleID) {
+    ERole(int roleID) {
         this.roleID = roleID;
     }
 
