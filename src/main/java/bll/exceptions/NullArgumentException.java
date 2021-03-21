dc68308d23ab4bcdda773a72c1577a7ee53a4f88
Jackson Barreto <jackson.barreto@gmail.com>
2021-03-21 18:51:59 +0000

    feat: Exception implemented.

---
+++ src/main/java/bll/exceptions/NullArgumentException.java
@@ -0,0 +1,10 @@
+package bll.exceptions;
+
+/**
+ * Thrown when a {@code null} value is received.
+ */
+public class NullArgumentException extends IllegalArgumentException {
+    public NullArgumentException() {
+        super();
+    }
+}
