1d2f6e76baeddc6973bd03305a47491c8ee3592a
Jackson Barreto <jackson.barreto@gmail.com>
2021-03-23 15:32:28 +0000

    Feat: EmptyArgumentException implemented.

---
+++ src/main/java/bll/exceptions/EmptyArgumentException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class EmptyArgumentException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public EmptyArgumentException() {
+        super();
+    }
+}
