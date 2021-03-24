54090ecf668708d47efff18acbf08d7da2ee6ed0
Jackson Barreto <jackson.barreto@gmail.com>
2021-03-24 18:09:09 +0000

    Feat: IvalidDescriptionSizeException implemented.

---
+++ src/main/java/bll/exceptions/InvalidDescriptionSizeException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class InvalidDescriptionSizeException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public InvalidDescriptionSizeException() {
+        super();
+    }
+}
