441844b7e187397680b8f35a607c45c50d91be38
Jackson Barreto <jackson.barreto@gmail.com>
2021-03-27 17:19:48 +0000

    Feat: DontsInstallmentException implemented.

---
+++ src/main/java/bll/exceptions/DontsInstallmentException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class DontsInstallmentException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public DontsInstallmentException() {
+        super();
+    }
+}
