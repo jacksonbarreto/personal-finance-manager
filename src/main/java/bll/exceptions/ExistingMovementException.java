3fb6424d5d711ae55bec470e9f6943ac5845ad82
Jackson Barreto <jackson.barreto@gmail.com>
2021-03-27 17:19:28 +0000

    Feat: ExistingMovementException implemented.

---
+++ src/main/java/bll/exceptions/ExistingMovementException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class ExistingMovementException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public ExistingMovementException() {
+        super();
+    }
+}
