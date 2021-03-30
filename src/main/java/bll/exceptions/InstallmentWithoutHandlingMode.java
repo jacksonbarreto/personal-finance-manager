e02a19a9f10cdfd524e95b540f3524f379047114
Jackson Barreto <jackson.barreto@gmail.com>
2021-03-30 11:57:24 +0100

    Feat: Exception implemented.

---
+++ src/main/java/bll/exceptions/InstallmentWithoutHandlingMode.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class InstallmentWithoutHandlingMode extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public InstallmentWithoutHandlingMode() {
+        super();
+    }
+}
---
+++ src/main/java/bll/exceptions/InvalidFormOfPaymentException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class InvalidFormOfPaymentException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public InvalidFormOfPaymentException() {
+        super();
+    }
+}
