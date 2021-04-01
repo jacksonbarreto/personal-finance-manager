dd0651d9f540c418257621aea14a20b9888f7a00
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-01 12:01:25 +0100

    Feat: Exception implemented.

---
+++ src/main/java/bll/exceptions/ProhibitedLessFormOfPayment.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class ProhibitedLessFormOfPayment extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public ProhibitedLessFormOfPayment() {
+        super();
+    }
+}
