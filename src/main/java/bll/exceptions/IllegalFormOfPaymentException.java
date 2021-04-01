986651a3d520fa0975f817519a883ac07c14ad11
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-01 14:42:43 +0100

    Refactor: fixed name of Exception.

--- src/main/java/bll/exceptions/IllegalFormOfPaymentException.java
+++ src/main/java/bll/exceptions/IllegalFormOfPaymentException.java
@@ -1,11 +1,11 @@
 package bll.exceptions;
 
-public class InvalidFormOfPaymentException extends IllegalArgumentException{
+public class IllegalFormOfPaymentException extends IllegalArgumentException{
     /**
      * Constructs an {@code IllegalArgumentException} with no
      * detail message.
      */
-    public InvalidFormOfPaymentException() {
+    public IllegalFormOfPaymentException() {
         super();
     }
 }
