873885c3be734a893bec01ee9b04d2c1f1de9025
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-02 11:13:25 +0100

    Feat: Exception implemented.

---
+++ src/main/java/bll/exceptions/IllegalInstallmentQuantityException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class IllegalInstallmentQuantityException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public IllegalInstallmentQuantityException() {
+        super();
+    }
+}
