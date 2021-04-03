710c8d43270ab952b4cac17eddf9a6a4bd5cfe30
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-03 11:44:08 +0100

    Feat: Exception implemented.

---
+++ src/main/java/bll/exceptions/AccessKeyAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class AccessKeyAlreadyExistsException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public AccessKeyAlreadyExistsException() {
+        super();
+    }
+}
---
+++ src/main/java/bll/exceptions/AccessKeyDoesNotExistException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class AccessKeyDoesNotExistException extends IllegalArgumentException {
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public AccessKeyDoesNotExistException() {
+        super();
+    }
+}
