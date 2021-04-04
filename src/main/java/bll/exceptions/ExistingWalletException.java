a8bba32962d5f0c109e4516796871c53d85e03c4
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-04 14:15:45 +0100

    Feat: Exception implemented.

---
+++ src/main/java/bll/exceptions/ExistingWalletException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class ExistingWalletException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public ExistingWalletException() {
+        super();
+    }
+}
