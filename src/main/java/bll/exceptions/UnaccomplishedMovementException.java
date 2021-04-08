5e39c6bd7a7bc76fed1c02f159702ffee6b59363
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-08 09:56:39 +0100

    Feat: Exception implemented.

---
+++ src/main/java/bll/exceptions/UnaccomplishedMovementException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class UnaccomplishedMovementException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public UnaccomplishedMovementException() {
+        super();
+    }
+}
