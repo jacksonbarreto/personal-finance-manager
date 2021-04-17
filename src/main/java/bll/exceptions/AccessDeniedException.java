4c51817ceccd087127c58db04c87662f1f76e3f5
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-17 19:16:05 +0100

    Feat: Exception implemented.

---
+++ src/main/java/bll/exceptions/AccessDeniedException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class AccessDeniedException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public AccessDeniedException() {
+        super();
+    }
+}
