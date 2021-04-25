27f9f26861eef569def93fe6c699918b20395165
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-25 20:46:29 +0100

    Feat: Exceptions.

---
+++ src/main/java/bll/exceptions/AccessDeniedUnconfirmedEmailException.java
@@ -0,0 +1,11 @@
+package bll.exceptions;
+
+public class AccessDeniedUnconfirmedEmailException extends IllegalArgumentException{
+    /**
+     * Constructs an {@code IllegalArgumentException} with no
+     * detail message.
+     */
+    public AccessDeniedUnconfirmedEmailException() {
+        super();
+    }
+}
