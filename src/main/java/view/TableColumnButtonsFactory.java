c44ca7ce054b246e88f67fb52edf42b111b22de4
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-27 12:01:55 +0100

    Feat: you can now add multiple buttons in the same column.

--- src/main/java/view/TableColumnButtonsFactory.java
+++ src/main/java/view/TableColumnButtonsFactory.java
@@ -1,39 +1,39 @@
 package view;
 
-import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.layout.HBox;
 import javafx.util.Callback;
 
-import java.util.List;
 
 public class TableColumnButtonsFactory<S> {
 
-    private final List<Button> buttons;
-    private final TableColumn<S, Button> column;
+    private final HBox buttons;
+    private final TableColumn<S, HBox> column;
 
-    public TableColumnButtonsFactory(TableColumn<S, Button> column, List<Button> buttons) {
+    public TableColumnButtonsFactory(TableColumn<S, HBox> column, HBox buttons) {
         this.buttons = buttons;
         this.column = column;
     }
 
-    public TableColumn<S, Button> getColumn() {
-        Callback<TableColumn<S, Button>, TableCell<S, Button>> cellFactory = new Callback<>() {
+    public TableColumn<S, HBox> getColumn() {
+        Callback<TableColumn<S, HBox>, TableCell<S, HBox>> cellFactory = new Callback<>() {
 
             @Override
-            public TableCell<S, Button> call(final TableColumn<S, Button> sButtonTableColumn) {
+            public TableCell<S, HBox> call(final TableColumn<S, HBox> sButtonTableColumn) {
                 return new TableCell<>() {
                     @Override
-                    public void updateItem(Button item, boolean empty) {
+                    public void updateItem(HBox item, boolean empty) {
                         super.updateItem(item, empty);
                         if (empty)
                             setGraphic(null);
                         else
-                            buttons.forEach(this::setGraphic);
+                            setGraphic(buttons);
                     }
                 };
             }
         };
+        this.column.setCellFactory(cellFactory);
         return this.column;
     }
 }
